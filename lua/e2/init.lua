0f0d7518a03db5d9ed7cddf58a03298aae4d8b6a
mrdgo <vmaxi1@ymail.com>
2024-10-01 14:23:36 +0200

    fix: add brach to spec

--- lua/e2/init.lua
+++ lua/e2/init.lua
@@ -1,30 +1,32 @@
 local function setup()
-	local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+  local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
 
-	parser_config.e2 = {
-		install_info = {
-			url = "https://github.com/mrdgo/tree-sitter-e2.git", -- local path or git repo
-			files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
-		},
-	}
+  parser_config.e2 = {
+    install_info = {
+      url = 'https://github.com/mrdgo/tree-sitter-e2.git', -- local path or git repo
+      files = { 'src/parser.c' }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+      branch = 'main',
+    },
+  }
 
-	parser_config.e2ir = {
-		install_info = {
-			url = "https://github.com/mrdgo/tree-sitter-e2ir.git", -- local path or git repo
-			files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
-		},
-	}
+  parser_config.e2ir = {
+    install_info = {
+      url = 'https://github.com/mrdgo/tree-sitter-e2ir.git', -- local path or git repo
+      files = { 'src/parser.c' }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+      branch = 'main',
+    },
+  }
 
-	vim.filetype.add({
-		extension = {
-			e2 = "e2",
-		},
-		pattern = {
-			[".*/e2compiler/.*.ir"] = "e2ir",
-		},
-	})
+  vim.filetype.add({
+    extension = {
+      e2 = 'e2',
+    },
+    pattern = {
+      ['.*/e2compiler/.*.ir'] = 'e2ir',
+    },
+  })
 end
 
 return {
-	setup = setup,
+  setup = setup,
 }
